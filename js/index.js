14dafa351e94d379b5efbd544ffbbbebc132f1bf
zss <18235183409@163.com>
2017-04-11 09:38:57 +0800

    uek1.0

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function()  
+{    
+     var designWidth=375;/*定义设计尺寸*/
+     function fontSize(){
+        var 	CW=document.documentElement.clientWidth; /*设备尺寸*
+	  /*console.log(CW); /输出文档的宽度  console.lg(CW);*/
+	   var size=CW/designWidth*100+"px";
+	   document.querySelector("html").style.fontSize=size;/*设置html的font-size*/
+      }
+	fontSize();/*运行函数*/
+	window.onresize=fontSize;/*监测窗口尺寸是否发生改变*/	
+}
